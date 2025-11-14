58bca2cc91e424a6aa5f7d4e4592ad009c56ca8b
Pedro Augusto Marinho <pedro.augustinho.marinho@hotmail.com>
2025-11-13 21:56:52 -0300

    Primeiro Projeto com SpringBoot

---
+++ BACK END/produtosapi/src/main/java/com/example/produtosapi/model/Produto.java
@@ -0,0 +1,50 @@
+package com.example.produtosapi.model;
+
+public class Produto {
+    private String nome;
+    private String id;
+    private String descricao;
+    private double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", id='" + id + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
